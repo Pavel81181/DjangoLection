1267c9feaade4765aa9820c4adee06cbf92458eb
Бажин Павел Аркадьевич <p.bazhin@agro.local>
2024-05-09 12:44:23 +0300

    2

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '192.168.1.47',
-    'p81181.pythonanywhere.com',
+    'Pavel81.pythonanywhere.com',
     ]
 
 INTERNAL_IPS = [
@@ -103,10 +103,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'p81181$default',
-            'USER': 'p81181',
+            'NAME': 'Pavel81$default',
+            'USER': 'Pavel81',
             'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-            'HOST': 'p81181.mysql.pythonanywhere-services.com',
+            'HOST': 'Pavel81.mysql.pythonanywhere-services.com',
             'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
